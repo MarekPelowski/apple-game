cf0e30fda34fa35ff992cd56a74a38e4316bbdea
Marek Pelowski <marek.pelowski@protonmail.com>
2023-06-05 21:31:45 +0200

    added the player

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ root.geometry("1000x500")
 root.configure(bg="black")
 root.resizable(False, False)
 
-apple_img = PhotoImage(file = r"apple.png")
+apple_img = PhotoImage(file=r"apple.png")
+player_img = PhotoImage(file=r"player.png")
 
+apple_image = Label(image=apple_img, borderwidth=0).pack()
 
+player_image = Label(image=player_img, borderwidth=0).pack()
 
 root.mainloop()
