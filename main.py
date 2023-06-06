6bb1d740e4573f20f35488dcc657b8f838833762
Marek Pelowski <marek.pelowski@protonmail.com>
2023-06-06 21:14:07 +0200

    made the player move in one direction

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 from tkinter import *
+from random import randint
 
 root = Tk()
 root.geometry("1000x500")
 root.configure(bg="black")
 root.resizable(False, False)
 
+x_apple = randint(0, 920)
+y_apple = randint(0, 420)
+
+x_player = 30
+y_player = 180
+
+player_speed = 7
+
+def moving(event):
+    global x_player
+    global y_player
+    if event.keysym == 'd':
+        x_player = x_player + player_speed
+        player.place(x=x_player)
+
+
 apple_img = PhotoImage(file=r"apple.png")
 player_img = PhotoImage(file=r"player.png")
 
-apple_image = Label(image=apple_img, borderwidth=0).pack()
+apple = Label(root, image=apple_img, borderwidth=0, bg="black")
+apple.place(x=x_apple, y=y_apple)
 
-player_image = Label(image=player_img, borderwidth=0).pack()
+player = Label(root, image=player_img, borderwidth=0, bg="black")
+player.place(x=x_player, y=y_player)
+
+root.bind("<Key>", moving)
+
 
 root.mainloop()
