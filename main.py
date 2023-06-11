39607253a261104fc44d3abc9924b49c7bf06682
Marek Pelowski <marek.pelowski@protonmail.com>
2023-06-11 21:30:14 +0200

    made the score and player can't get through the border

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from tkinter import *
-from random import randint, randrange
+from random import randrange
 
 root = Tk()
 root.geometry("1000x500")
 root.configure(bg="black")
 root.resizable(False, False)
+root.title("Apple Game")
 
+score_number = 0
 
+score = IntVar()
+score.set(score_number)
 
 x_apple = randrange(0, 920, 5)
 y_apple = randrange(0, 420, 5)
 
-x_player = 0
-y_player = 0
+x_player = 100
+y_player = 210
 
 player_speed = 5
 
@@ -21,6 +25,18 @@ player_right_img = PhotoImage(file=r"player_right.png")
 player_left_img = PhotoImage(file=r"player_left.png")
 player_up_img = PhotoImage(file=r"player_up.png")
 player_down_img = PhotoImage(file=r"player_down.png")
+
+def hitting_border():
+    global y_player, x_player
+
+    if y_player == 0:
+        y_player = y_player + 50
+    elif y_player == 420:
+        y_player = y_player - 50
+    elif x_player == 0:
+        x_player = x_player + 50
+    elif x_player == 920:
+        x_player = x_player - 50
 
 
 def make_apple():
@@ -39,32 +55,35 @@ def moving(event):
     global y_player
 
     hitting_apple()
+    hitting_border()
 
     if event.keysym == 'w':
         y_player = y_player - player_speed
         player.place(y=y_player)
         player.configure(image=player_up_img)
-        hitting_apple()
+
 
     elif event.keysym == 'a':
         x_player = x_player - player_speed
         player.place(x=x_player)
         player.configure(image=player_left_img)
-        hitting_apple()
+
 
     elif event.keysym == 's':
         y_player = y_player + player_speed
         player.place(y=y_player)
         player.configure(image=player_down_img)
-        hitting_apple()
+
 
     elif event.keysym == 'd':
         x_player = x_player + player_speed
         player.place(x=x_player)
         player.configure(image=player_right_img)
-        hitting_apple()
+
 
 def hitting_apple():
+    global score_number
+
 
 
 # from the left
@@ -72,9 +91,13 @@ def hitting_apple():
     if x_player == (x_apple - 40) and y_player >= y_apple and y_player < (y_apple + 40):
         apple.destroy()
         make_apple()
+        score_number = score_number + 1
+        score.set(score_number)
     elif x_player == (x_apple - 40) and y_player <= y_apple and y_player > (y_apple - 85):
         apple.destroy()
         make_apple()
+        score_number = score_number + 1
+        score.set(score_number)
 
 
 # from the bottom
@@ -82,9 +105,13 @@ def hitting_apple():
     elif x_player >= x_apple and x_player <= (x_apple + 40) and y_player == (y_apple + 40):
         apple.destroy()
         make_apple()
+        score_number = score_number + 1
+        score.set(score_number)
     elif x_player <= x_apple and x_player >= (x_apple - 40) and y_player == (y_apple + 40):
         apple.destroy()
         make_apple()
+        score_number = score_number + 1
+        score.set(score_number)
 
 
 # from the top
@@ -92,10 +119,14 @@ def hitting_apple():
     elif x_player >= x_apple and x_player <= (x_apple + 40) and y_player == (y_apple - 80):
         apple.destroy()
         make_apple()
+        score_number = score_number + 1
+        score.set(score_number)
 
     elif x_player <= x_apple and x_player >= (x_apple - 40) and y_player == (y_apple - 80):
         apple.destroy()
         make_apple()
+        score_number = score_number + 1
+        score.set(score_number)
 
 
 
@@ -104,10 +135,12 @@ def hitting_apple():
     if x_player == (x_apple + 40) and y_player >= y_apple and y_player < (y_apple + 40):
         apple.destroy()
         make_apple()
+        score.set(score_number + 1)
 
     elif x_player == (x_apple + 40) and y_player <= y_apple and y_player > (y_apple - 85):
         apple.destroy()
         make_apple()
+        score.set(score_number + 1)
 
 
 
@@ -118,6 +151,12 @@ apple.place(x=x_apple, y=y_apple)
 
 player = Label(root, image=player_right_img, borderwidth=0, bg="black")
 player.place(x=x_player, y=y_player)
+
+score1_label = Label(root, text="score: ", font=("Arial", 20, "bold"), bg="white", fg="black")
+score1_label.place(x=20, y=20)
+
+score2_label = Label(root, textvariable=score, font=("Arial", 20, "bold"), bg="white", fg="black")
+score2_label.place(x=110, y=20)
 
 root.bind("<Key>", moving)
 
